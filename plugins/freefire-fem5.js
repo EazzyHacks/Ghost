c116c62981c31019925f0e749f8fd7454fce3cb2
EazzyHacks <yhair7200@gmail.com>
2025-08-14 11:20:08 +0200

    Update freefire-fem5.js

--- plugins/freefire-fem5.js
+++ plugins/freefire-fem5.js
@@ -61,7 +61,7 @@ let handler = async (m, { conn, args }) => {
 
 handler.help = ['Fem5']
 handler.tags = ['ffsurr']
-handler.command = /^(Fem5)$/i
+handler.command = ['Fem5']
 handler.group = true
 handler.admin = true
 
