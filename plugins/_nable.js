263ce2ce51943f9b69d9b3875cced29445056ef8
EazzyHacks <yhair7200@gmail.com>
2025-08-14 04:52:01 -0500

    Update _nable.js

--- plugins/_nable.js
+++ plugins/_nable.js
@@ -112,9 +112,7 @@ const handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, i
   }
 }
 
-handler.help = ['welcome', 'bienvenida', 'antiprivado', 'antiprivate', 'restrict', 'restringir', 'autolevelup', 'autonivel', 'antibot', 'antibots', 'autoaceptar', 'aceptarauto', 'autorechazar', 'rechazarauto', 'autoresponder', 'autorespond', 'antisubbots', 'antibot2', 'modoadmin', 'soloadmin', 'reaction', 'reaccion', 'nsfw', 'modohorny', 'antispam', 'jadibotmd', 'modejadibot', 'subbots', 'detect', 'avisos', 'antilink', 'antifake' 'enable']
-handler.tags = ['nable']
-handler.command = handler.help
+handler.help = ['welcome', 'bienvenida', 'antiprivado', 'antiprivate', 'restrict', 'restringir', 'autolevelup', 'autonivel', 'antibot', 'antibots', 'autoaceptar', 'aceptarauto', 'autorechazar', 'rechazarauto', 'autoresponder', 'autorespond', 'antisubbots', 'antibot2', 'modoadmin', 'soloadmin', 'reaction', 'reaccion', 'nsfw', 'modohorny', 'antispam', 'jadibotmd', 'modejadibot', 'subbots', 'detect', 'avisos', 'antilink', 'antifake']
 handler.command = ['enable', 'on', 'disable','off']
 
 export default handler
